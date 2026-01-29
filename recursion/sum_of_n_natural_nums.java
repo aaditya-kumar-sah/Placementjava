7ebf9af84c01eaa780e05d0a2d2c7921fcb435fd
aadityasah <shahsudha259@gmail.com>
2026-01-29 19:23:16 +0545

    factorial and sum of n natural nums updated

---
+++ recursion/sum_of_n_natural_nums.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class recursion{
+    public static void printSum(int n, int i, int sum){
+        if(i == n){
+            sum += i;
+            System.out.println(sum);
+            return;
+        }
+        sum += i;
+        printSum(n, i+1, sum);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printSum(n, 1, 0);
+    }
+}
