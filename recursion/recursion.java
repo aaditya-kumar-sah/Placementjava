2fa0867a1af04efa4d662210c83b1c1dc7d685bc
aadityasah <shahsudha259@gmail.com>
2026-01-29 18:57:49 +0545

    recursion 1-5 print

---
+++ recursion/recursion.java
@@ -0,0 +1,13 @@
+public class recursion{
+    public static void printNum(int n){
+        if(n == 0){
+            return;
+        }
+        printNum(n-1);
+        System.out.println(n);
+    }
+    public static void main(String args[]){
+        int n = 5;
+        printNum(n);
+    }
+}
--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -1,13 +1,16 @@
-public class bubble {
+package sort;
+
+public class BubbleSort {
     public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + "");
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
 
     public static void main(String args[]) {
-        int arr[] = { 4, 5, 2, 1 };
+        int arr[] = {4, 5, 2, 1};
+
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
