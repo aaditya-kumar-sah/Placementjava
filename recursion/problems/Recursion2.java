6147027dec700f206b5e6e435d4ddf8ef41668bc
aadityasah <shahsudha259@gmail.com>
2026-01-30 09:18:12 +0545

    push

---
+++ recursion/problems/Recursion2.java
@@ -0,0 +1,15 @@
+public class Recursion2{
+    public static void hanoi(int n, String src, String help, String dest){
+        if(n == 1){
+            System.out.println("transfer disk" + n + "from" + src + "to" + dest);
+            return;
+        }
+        hanoi(n-1, src, dest, help);
+        System.out.println("transfer disk" + n + "from" + src + "to" + dest);
+        hanoi(n-1, help, src, dest);
+    }
+    public static void main(String args[]){
+        int n = 1;
+        hanoi(n, "S", "H", "D");
+    }
+}
