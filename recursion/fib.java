7da516696567c731e56b34958f6d6ccd6b9766bc
aadityasah <shahsudha259@gmail.com>
2026-01-30 10:23:33 +0545

    finonacci series in java

---
+++ recursion/fib.java
@@ -0,0 +1,18 @@
+public class fib {
+    public static void ser(int a, int b, int n){
+        if(n == 0){
+            return;
+        }
+        int c = a + b;
+        System.out.println(c);
+        ser(b, c, n-1);
+    }
+    public static void main(String args[]){
+        int a = 0;
+        int b = 1;
+        System.out.println(a);
+        System.out.println(b);
+        int n = 7;
+        ser(a, b, n - 2);
+    }
+}
