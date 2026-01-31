dd41e13cc5dbb86184e7cc14c0b341d4b18bb33e
aadityasah <shahsudha259@gmail.com>
2026-01-31 21:42:46 +0545

    linkedlist updated

---
+++ linkedlist/LL.java
@@ -0,0 +1,63 @@
+package linkedlist;
+public class LL{
+
+    Node head;
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+    }   
+
+    // add first
+    public void addFirst(String data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // add last
+    public void addLast(String data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while(currNode.next != null){
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    // print
+    public void printList(){
+        if(head == null){
+            System.out.println("List is empty");
+            return;
+        }
+        Node curNode = head;
+        while(curNode != null){
+            System.out.print(curNode.data + " -> ");
+            curNode = curNode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String args[]){
+        LL list = new LL();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.addLast("list");
+
+        list.printList();
+    }
+}
