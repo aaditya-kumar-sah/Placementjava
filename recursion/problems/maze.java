464822f9b8859e4a01f192c75a53330d2377d02c
aadityasah <shahsudha259@gmail.com>
2026-02-01 08:49:43 +0545

    rat maze 4 direction

--- recursion/problems/maze.java
+++ recursion/problems/maze.java
@@ -4,15 +4,37 @@ public class maze {
     public static void main(String args[]){
         int rows = 3;
         int cols = 3;
-        print(1, 1, rows, cols, "");
+        boolean[][] isVisited = new boolean[rows][cols]; // by default false hai sara ka sara
+        print(0, 0, rows - 1, cols - 1, "", isVisited); 
     }
-    public static void print(int sr, int sc, int er, int ec, String s){
+    public static void print(int sr, int sc, int er, int ec, String s, boolean[][] isVisited){
+        
+        if(sr < 0 || sc < 0) return;
+        
         if(sr > er || sc > ec) return;
+
+        if(isVisited[sr][sc] == true) return;
+        
         if(sr == er && sc == ec){
             System.out.println(s);
             return;
         }
-        print(sr + 1, sc, er, ec, s + "D");
-        print(sr, sc + 1, er, ec, s + "R");
+
+        isVisited[sr][sc] = true;
+
+        // go right
+        print(sr + 1, sc, er, ec, s + "D", isVisited);
+
+        // go down
+        print(sr, sc + 1, er, ec, s + "R", isVisited);
+
+        // go left
+        print(sr, sc - 1, er, ec, s + "L", isVisited);
+
+        // go up
+        print(sr - 1, sc - 1, er, ec, s + "U", isVisited);
+
+        // backtracking 
+        isVisited[sr][sc] = false;
     }
 }
