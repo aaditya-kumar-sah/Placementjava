c17451d93564f86135af268de7555750bd91f266
aadityasah <shahsudha259@gmail.com>
2026-02-14 15:14:50 +0545

    puhs

--- size.java
+++ size.java
@@ -1,27 +1,32 @@
 public class size {
     public static class Node{
-        int val;
+        int val; 
         Node left;
         Node right;
+
         public Node(int val){
             this.val = val;
         }
     }
-    public static void display(Node root){
-        if(root == null) return;
-        System.out.print(root.val + " ->");
-        if(root.left != null) System.out.print("(" + root.left.val + " ,");
-        if(root.right != null) System.out.println(root.right.val + ")");
-        display(root.left);
-        display(root.right);
+    public static int max(Node root){
+        if(root == null) return 0;
+        int a = root.val;
+        int b = max(root.left);
+        int c = max(root.right);
+        return Math.max(a, Math.max(b, c));
+    }
+    public static int size(Node root){
+        if(root == null) return 0;
+        return root.val + size(root.left) + size(root.right);
     }
     public static void main(String args[]){
-        Node root = new Node(2);
-        Node a = new Node(4);
-        Node b = new Node(10);
-        Node c = new Node(6);
+        Node root = new Node(1);
+        Node a = new Node(2);
+        Node b = new Node(3);
+        Node c = new Node(4);
         Node d = new Node(5);
-        Node e = new Node(1);
+        Node e = new Node(6);
+        Node f = new Node(9);
         root.left = a; 
         root.right = b;
         
@@ -29,7 +34,9 @@ public class size {
         a.right = d;
         
         b.right = e;
+        b.left = f;
 
-        display(root);
+        System.out.println(size(root));
+        System.out.println(max(root));
     }
 }
