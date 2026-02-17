d30ad1baf8b8f5a89d39eac0b106db6d0d58f2d7
aadityasah <shahsudha259@gmail.com>
2026-02-17 17:05:36 +0545

    practice

--- practice.java
+++ practice.java
@@ -1,24 +1,22 @@
-package graphs;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class BFSgraph {
-
-    static class Edge {
+public class practice{
+    static class Edge{
         int src;
         int dest;
 
-        public Edge(int s, int d) {
+        public Edge(int s, int d){
             this.src = s;
             this.dest = d;
         }
     }
 
-    public static void CreateGraph(ArrayList<Edge> graph[]) {
+    public static void CreateGraph(ArrayList<Edge> graph[]){
         for(int i = 0; i < graph.length; i ++){
-            graph[i] = new ArrayList<Edge>();
+            graph[i] = new ArrayList<>();
         }
-
         graph[0].add(new Edge(0, 1));
         graph[0].add(new Edge(0, 2));
 
@@ -42,12 +40,14 @@ public class BFSgraph {
         graph[5].add(new Edge(5, 5)); 
     }
 
+
+
     public static void bfs(ArrayList<Edge> graph[], boolean[] vis, int start){
         Queue<Integer> q = new LinkedList<>();
         q.add(start);
         while(!q.isEmpty()){
             int curr = q.remove();
-            if(vis[curr] == false){
+            if(!vis[curr]){
                 System.out.println(curr);
                 vis[curr] = true;
                 for(int i = 0; i < graph[curr].size(); i ++){
@@ -57,15 +57,15 @@ public class BFSgraph {
             }
         }
     }
-    public static void main(String args[]) {
-        int V = 7;
-        ArrayList<Edge>[] graph = new ArrayList[V];
 
+    public static void main(String args[]){
+        int V = 7;
+        ArrayList<Edge> graph[] = new ArrayList[V];
         CreateGraph(graph);
         boolean[] vis = new boolean[V];
         for(int i = 0; i < V; i ++){
             if(vis[i] == false){
-                bfs(graph, V, vis, i);
+                bfs(graph, vis, i);
             }
         }
     }
