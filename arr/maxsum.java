be98fe5072c75fbb7511ec32ebefb9fe911a4187
aadityasah <shahsudha259@gmail.com>
2026-02-21 11:30:15 +0545

    sliding window const

---
+++ arr/maxsum.java
@@ -0,0 +1,18 @@
+package arr;
+public class maxsum{
+
+    public static void main(String args[]){
+        int[] nums = {2,1,1,3,2};
+        int k = 3;
+        int n = nums.length, currSum = 0, maxSum;
+        for(int i = 0; i < k; i++){
+            currSum += nums[i];
+        }
+        maxSum = currSum;
+        for(int i = k; i < n; i ++){
+            currSum = currSum - nums[i - k] + nums[i];
+            maxSum = Math.max(maxSum, currSum);
+        }
+        System.out.println(maxSum);
+    }
+}
