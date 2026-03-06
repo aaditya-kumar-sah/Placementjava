c47e651a76ddd1f41a7bf793d45e2dc9b8b13902
aadityasah <shahsudha259@gmail.com>
2026-03-06 21:17:54 +0545

    BT

---
+++ allabtBT/bfsinTree.java
@@ -0,0 +1,39 @@
+package allabtBT;
+public class bfsinTree {
+
+    static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        public Node(int val){
+            this.val = val;
+        }
+    }
+
+    public static void printuptonthnode(Node root, int n){
+        if(root == null) return;
+        if(n == 1) System.out.println(root.val);
+        printuptonthnode(root.left, n - 1);
+        printuptonthnode(root.right, n - 1);
+    }
+    public static int height(Node root){
+        if(root == null) return -1;  // height of empty tree = -1
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+    public static void main(String args[]) {
+
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(2);
+
+        root.left.left = new Node(3);
+        root.left.right = new Node(4);
+
+        root.right.left = new Node(4);
+        root.right.right = new Node(3);
+        printuptonthnode(root, 2);
+
+    }
+}
---
+++ bfsinTree.java
@@ -0,0 +1,43 @@
+import graphs.dfs;
+
+public class bfsinTree {
+
+    static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        public Node(int val){
+            this.val = val;
+        }
+    }
+    // to traverse in BT
+    public static void dfs(Node root){
+        if(root == null) return;
+        System.out.println(root.val);
+        dfs(root.left);
+        dfs(root.right);
+    }
+    // 
+    public void find(Node root, int target){
+        if(root == null) return;
+
+        if(root.val == target) return root;
+    }
+
+    public static void main(String args[]) {
+        int targer = 4;
+
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(2);
+
+        root.left.left = new Node(3);
+        root.left.right = new Node(4);
+
+        root.right.left = new Node(4);
+        root.right.right = new Node(3);
+        // dfs(root);
+    }
+}
